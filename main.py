305013e5cd5d4891b7106e5ff7d5650fde088b9f
Kamila Mattos <99353050+KamilaMattos@users.noreply.github.com>
2022-09-22 13:43:17 -0300

    fix: trying pass in tests

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from utils.json_handler import write_json
 from management.tab_handler import calculate_tab
 
-file_path = 'menu.json'
+FILEPATH = 'menu.json'
 
 
 def main():
     new_item = {"name": "CHURROS DO M5", "price": 5.0}
     write_json('menu.json', new_item)
 
-    table_1 = [{'id': 1, 'amount': 5}, {'id': 19, 'amount': 5}]
+    TABLE_1 = [{'id': 1, 'amount': 5}, {'id': 19, 'amount': 5}]
 
-    return calculate_tab(table_1, file_path)
+    return calculate_tab(TABLE_1, FILEPATH)
 
 
 if __name__ == "__main__":
--- management/tab_handler.py
+++ management/tab_handler.py
@@ -2,22 +2,22 @@ from datetime import datetime
 from utils.json_handler import read_json
 
 
-def get_item_menu(path: str, id_item_table: int):
-    menu = read_json(path)
+def get_item_menu(filepath: str, id_item_table: int):
+    menu = read_json(filepath)
 
     for items in menu:
         if items['id'] == id_item_table:
             return items
 
 
-def calculate_tab(table: list[dict], path: str) -> dict:
+def calculate_tab(table: list[dict], filepath: str):
     date_time_formated = '%d/%m/%Y, %H:%M:%S'
     subtotal = 0
 
     for items in table:
         id_item_table = items['id']
         amount = items['amount']
-        item_menu = get_item_menu(path, id_item_table)
+        item_menu = get_item_menu(filepath, id_item_table)
         price = item_menu['price']
 
         subtotal += price * amount
